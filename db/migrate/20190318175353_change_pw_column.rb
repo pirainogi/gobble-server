b5bbf13b2bed59ac9c853a502cb17b1851c20d4e
pirainogi <pirainogi@gmail.com>
2019-03-18 13:57:22 -0400

    pw digest

---
+++ db/migrate/20190318175353_change_pw_column.rb
@@ -0,0 +1,6 @@
+class ChangePwColumn < ActiveRecord::Migration[5.2]
+  def change
+    change_table :users do |t|
+      t.rename :password, :password_digest
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_03_13_214536) do
+ActiveRecord::Schema.define(version: 2019_03_18_175353) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -116,7 +116,7 @@ ActiveRecord::Schema.define(version: 2019_03_13_214536) do
   create_table "users", force: :cascade do |t|
     t.string "name"
     t.string "username"
-    t.string "password"
+    t.string "password_digest"
     t.string "profile_pic"
     t.text "bio"
     t.string "allergies"
