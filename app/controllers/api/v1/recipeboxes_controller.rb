5931271dcd69a901c197d6586f09d4492035d53b
pirainogi <pirainogi@gmail.com>
2019-03-21 10:47:07 -0400

    fixed rb controller

--- app/controllers/api/v1/recipeboxes_controller.rb
+++ app/controllers/api/v1/recipeboxes_controller.rb
@@ -10,11 +10,12 @@ class Api::V1::RecipeboxesController < ApplicationController
     render json: @recipebox, status: :accepted
   end
 
-  def new
-    @recipebox = Recipebox.new
-  end
+  # def new
+  #   @recipebox = Recipebox.new
+  # end
 
   def create
+    byebug
     @recipebox = Recipebox.create(recipebox_params)
     if @recipebox.valid?
       render json: @recipebox, status: :accepted
@@ -23,26 +24,26 @@ class Api::V1::RecipeboxesController < ApplicationController
     end
   end
 
-  def edit
-    @recipebox = Recipebox.find(params[:id])
-    @recipebox.update(recipebox_params)
-    render json: @recipebox, status: :accepted
-  end
-
-  def update
-    @recipebox.update(recipebox_params)
-    if @recipebox.save
-      render json: @recipebox, status: :accepted
-    else
-      render json: { errors: @recipebox.errors.full_messages }, status: :unprocessible_entity
-    end
-  end
+  # def edit
+  #   @recipebox = Recipebox.find(params[:id])
+  #   @recipebox.update(recipebox_params)
+  #   render json: @recipebox, status: :accepted
+  # end
+  #
+  # def update
+  #   @recipebox.update(recipebox_params)
+  #   if @recipebox.save
+  #     render json: @recipebox, status: :accepted
+  #   else
+  #     render json: { errors: @recipebox.errors.full_messages }, status: :unprocessible_entity
+  #   end
+  # end
 
 
 private
 
 def recipebox_params
-  params.permit(:name)
+  params.require(:recipebox).permit(:recipe_id, :user_id)
 end
 
 end
