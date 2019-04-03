3b7cb5b13d17e6654347c4d5896b8943fb8be9a0
pirainogi <pirainogi@gmail.com>
2019-04-03 15:46:55 -0400

    events serializer

--- app/serializers/user_serializer.rb
+++ app/serializers/user_serializer.rb
@@ -2,9 +2,14 @@ class UserSerializer < ActiveModel::Serializer
   attributes :id, :name, :email, :password_digest, :profile_pic, :bio, :allergies, :diet
 
   has_many :recipes
+  has_many :events
 
   class RecipeSerializer < ActiveModel::Serializer
     attributes :id, :name, :imgurl, :instructions, :preptime, :dishType, :sourceUrl, :servings, :whole30, :keto, :dairyFree, :glutenFree, :vegan, :vegetarian, :healthy, :ingredients
+  end
+
+  class EventSerializer < ActiveModel::Serializer
+    attributes :id, :eventStart, :eventEnd, :eventType, :name
   end
 
 end
